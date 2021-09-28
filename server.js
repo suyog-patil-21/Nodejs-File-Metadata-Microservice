a41245e16c8230023a94a8610d46431ac2c3c0e3
MyUbuntu <suyog0057@gmail.com>
2021-09-28 23:00:15 +0530

    complete

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+var multer = require("multer");
 
 var app = express();
+var fileStorageEngine = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log("destination :", file);
+    cb(null, process.cwd() + "/upload");
+  },
+  filename: function (req, file, cb) {
+    console.log("filename :", file);
+    cb(null, file.originalname);
+  },
+});
+var upload = multer({ storage: fileStorageEngine });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  try {
+    var filedata = req.file;
+    res.json({
+      name: filedata.originalname,
+      type: filedata.mimetype,
+      size: filedata.size,
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
